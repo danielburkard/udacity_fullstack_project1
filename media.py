6625ab790535b3653c24312e288fb9950441f841
Daniel Burkard <danielburkard@me.com>
2015-02-11 02:13:58 +0100

    Update media.py

--- media.py
+++ media.py
@@ -1,16 +1,16 @@
 # definition for class movie
 class Movie:
 	""" This class provides a way to store movie related information"""
-	# constructor with title,Box Art URL and YouTube URL
 	def __init__(self,title,poster_image_url,trailer_youtube_url):
+		""" constructor with title,Box Art URL and YouTube URL"""
 		self.title = title
 		self.poster_image_url = poster_image_url
 		self.trailer_youtube_url = trailer_youtube_url
 
-	# adds a list of actors
 	def addActors(self,actors):
+		"""adds a list of actors"""
 		self.actors = actors
 
-	# adds a release date
 	def addReleaseDate(self,date):
+		"""adds a release date"""
 		self.releaseDate = date
